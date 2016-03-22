1d5b56661d589fa6dca8834fb55eb7533eb9bc92
Francesco Paolo Lovergine <frankie@debian.org>
2016-03-22 17:20:04 +0100

    Added license snippet and file.

--- smap2shp.py
+++ smap2shp.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 #
-# This little script converts SMAP HDF5 files in ESRI shapefile
-# by exctracting only soil moisture values. 
-# 
+#   This little script converts SMAP HDF5 files in ESRI shapefile
+#   by exctracting only soil moisture values. 
+#
+#   Copyright (C) 2016 Francesco P. Lovergine <francesco.lovergine@cnr.it>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 import sys
 import os
